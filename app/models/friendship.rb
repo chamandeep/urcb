eae0b5f45f99d04b4e825f7cdadfc9ad66f06bed
= <k1016113@kingston.ac.uk>
2014-02-14 20:59:56 +0000

    added assocations for friendhsip model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,5 @@
 class Friendship < ActiveRecord::Base
+belongs_to :user
+belongs_to :friend, :class_name => "User", :foreign_key => "friend_id" 
+  validates_presence_of :user_id, :friend_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 has_many :Educations
 has_many :Disabilities, through: :UserDisability
+has_many :Friendships
 end
