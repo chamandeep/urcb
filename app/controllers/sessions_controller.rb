61556b125b7f644799e1b02fdf91d384c766f1f7
= <k1016113@kingston.ac.uk>
2014-03-22 18:45:17 +0000

    added redirect to user show page after login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ end
 def create
   @user = User.find_by(email: params[:session][:email].downcase)
   if @user && @user.authenticate(params[:session][:password])
-
 login @user
+redirect_to @user
      else
         flash.now[:error] = 'Invalid email/password combination'
       render 'new'
