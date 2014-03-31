2266c945bf45f7340312b238638e1ec7d2b2cd10
= <k1016113@kingston.ac.uk>
2014-03-31 22:13:07 +0100

    added the actions in the friendships controller to mae the follow/unfollow buttons work. a loged in user can navigate to another user's profile and click on the follow/unfollow button.

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,15 @@
+class FriendshipsController < ApplicationController
+  before_action :loged_in_user
+
+  def create
+    @user = User.find(params[:friendship][:followed_id])
+    current_user.follow!(@user)
+    redirect_to @user
+  end
+
+  def destroy
+    @user = Friendship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    redirect_to @user
+  end
+end
