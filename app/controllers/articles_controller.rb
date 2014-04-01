aa45c3371acf3a0b70d1eef10a5aead3f834855b
= <k1016113@kingston.ac.uk>
2014-04-01 20:27:28 +0100

    added user permissions. another user cannot edit or delete an article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :loged_in_user, only: [:edit, :index, :update, :destroy, :following, :followers]
+    before_action :correct_user,   only: [:edit, :update]
 
   # GET /articles
   def index
