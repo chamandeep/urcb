468d7179e9574985e6ed67fb0a749d952517ab86
= <k1016113@kingston.ac.uk>
2014-04-08 01:22:36 +0100

    sorted article listing by most recently updated first.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   
-  
+#order by recently updated
+default_scope -> { order('updated_at DESC') }  
   #method to retreve all articles which have been approved
   def approved
     self.status === 2
