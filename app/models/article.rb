b34b84879ac20235edc78b7ecf8f7d6c802cd245
= <k1016113@kingston.ac.uk>
2014-04-10 22:00:11 +0100

    added approve and reject buttons on each article page. status is changed to 3 when approved and 2 for rejected and 1 for submitted. this is done by sending a hidden field with the relevant value and the update action takes care of the update.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,9 +3,7 @@ class Article < ActiveRecord::Base
   
 #order by recently updated
 default_scope -> { order('updated_at DESC') }  
-  #method to retreve all articles which have been approved
-  def approved
-    self.status === 2
-  end
+  
+
   
 end
