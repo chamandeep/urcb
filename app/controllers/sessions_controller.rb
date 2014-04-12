4df9088c7fb55be634474f452407a2ce26b77cb5
= <k1016113@kingston.ac.uk>
2014-04-12 15:23:51 +0100

    redirect to home page after login instead of profile.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ def create
   @user = User.find_by(email: params[:session][:email].downcase)
   if @user && @user.authenticate(params[:session][:password])
 login @user
-redirect_to @user
+redirect_to root_url
   else
         flash.now[:error] = 'Invalid email/password combination'
       render 'new'
