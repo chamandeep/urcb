b700ae4a4c30bcf62ba8ae71033eb71fd4e3a435
= <k1016113@kingston.ac.uk>
2014-04-12 23:22:05 +0100

    added the ability to rate a university and show the average on the university page.

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,4 +1,6 @@
 class Education < ActiveRecord::Base
+  after_save :updateUniversityRatings
+  
   belongs_to :user
   belongs_to :course
   belongs_to :university
@@ -7,4 +9,8 @@ class Education < ActiveRecord::Base
   
     validates :university_id, presence: true
       validates :course_id, presence: true
+      #method to run the update method in the university model 
+      def updateUniversityRatings
+        university.updateDisabilityDeptRating
+      end
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,3 +1,15 @@
 class University < ActiveRecord::Base
 has_many :educations
+
+#method to update the average rating. This method will be called each time an education record is added or updated
+def updateDisabilityDeptRating
+      value = 0.00
+    self.educations.each do |education|
+      value = value + education.disabilityDeptRating 
+    end
+    total = self.educations.size
+    update_attributes(disabilityDeptRating: value.to_f / total.to_f)
+  end
+
+
 end
